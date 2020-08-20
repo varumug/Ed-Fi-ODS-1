590f2f45af4e36f0d96fa3fd833177a504df488f
Jim McKay <jmckay@certicasolutions.com>
2020-08-20 12:26:08 -0500

    removed admin tests

--- Application/EdFi.Ods.NetFramework.Tests/EdFi.Ods.Admin/Extensions/StringExtensionsTests.cs
+++ Application/EdFi.Ods.NetFramework.Tests/EdFi.Ods.Admin/Extensions/StringExtensionsTests.cs
@@ -3,42 +3,42 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using EdFi.Ods.Admin.Extensions;
-using NUnit.Framework;
-using Shouldly;
-
-namespace EdFi.Ods.Tests.EdFi.Ods.Admin.Extensions
-{
-    public class StringExtensionsTests
-    {
-        [TestFixture]
-        public class When_email_address_is_valid
-        {
-            [Test]
-            public void Should_indicate_address_is_valid()
-            {
-                var address = "foo.bar@example.com";
-
-                address.IsValidEmailAddress()
-                       .ShouldBeTrue();
-            }
-        }
-
-        [TestFixture]
-        public class When_email_address_is_invalid
-        {
-            [Test]
-            public void Should_indicate_address_is_invalid()
-            {
-                "missingdomain".IsValidEmailAddress()
-                               .ShouldBeFalse();
-
-                "missingdomain@".IsValidEmailAddress()
-                                .ShouldBeFalse();
-
-                "@example.com".IsValidEmailAddress()
-                              .ShouldBeFalse();
-            }
-        }
-    }
-}
+// using EdFi.Ods.Admin.Extensions;
+// using NUnit.Framework;
+// using Shouldly;
+//
+// namespace EdFi.Ods.Tests.EdFi.Ods.Admin.Extensions
+// {
+//     public class StringExtensionsTests
+//     {
+//         [TestFixture]
+//         public class When_email_address_is_valid
+//         {
+//             [Test]
+//             public void Should_indicate_address_is_valid()
+//             {
+//                 var address = "foo.bar@example.com";
+//
+//                 address.IsValidEmailAddress()
+//                        .ShouldBeTrue();
+//             }
+//         }
+//
+//         [TestFixture]
+//         public class When_email_address_is_invalid
+//         {
+//             [Test]
+//             public void Should_indicate_address_is_invalid()
+//             {
+//                 "missingdomain".IsValidEmailAddress()
+//                                .ShouldBeFalse();
+//
+//                 "missingdomain@".IsValidEmailAddress()
+//                                 .ShouldBeFalse();
+//
+//                 "@example.com".IsValidEmailAddress()
+//                               .ShouldBeFalse();
+//             }
+//         }
+//     }
+// }
